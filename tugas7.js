42639ee6d42530f178668dde6c27763f03afb8fb
wildan <wildan.syarif1021@gmail.com>
2019-11-07 14:50:43 +0700

    tugas niomic
    tugas niomic

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+let arr = [162, 174, 181, 159, 201, 168, 175, 182, 176, 171];
+
+console.log('perulangan biasa: ');
+for(let i=0; i<10; i++){
+ console.log(arr[i]);
+}
+
+console.log('perulangan for off: ');
+for(let i=0; i<10; i++){
+ console.log(arr[i]);
+}
